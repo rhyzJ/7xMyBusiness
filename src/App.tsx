3fd0e1d0fc97b24bdc6168aa9de5600293d497ea
Rhya Johns <165769330+rhyzJ@users.noreply.github.com>
2025-12-14 09:38:45 +1300

    refactor: switch from HashRouter to BrowserRouter for improved routing and update Contact page path for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="contact" element={<Contact />} />
       </Route>
     </Routes>
   );
