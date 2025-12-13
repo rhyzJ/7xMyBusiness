28457c812f8691c061de3a7808f56868968447a3
Rhya Johns <165769330+rhyzJ@users.noreply.github.com>
2025-12-13 21:47:39 +1300

    fix: update BrowserRouter basename for correct routing in production

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // <-- import router
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/7xMyBusiness/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
